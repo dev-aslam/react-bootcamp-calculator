1939365ffd8f13569b698bdc548fb800b7304efe
p-h-0-e-n-i-x <muhammedaslam204@gmail.com>
2022-11-03 23:25:23 +0530

    created calculator layouts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <section className="monitor">
+          <div className="input-data">
+            25*25
+          </div>
+          <div className="result">
+            625
+          </div>
+        </section>
+        <section className="input-button">
+          <button className="numbers">7</button>
+          <button className="numbers">8</button>
+          <button className="numbers">9</button>
+          <button className="symbols">/</button>
+          <button className="numbers">4</button>
+          <button className="numbers">5</button>
+          <button className="numbers">6</button>
+          <button className="symbols">*</button>
+          <button className="numbers">1</button>
+          <button className="numbers">2</button>
+          <button className="numbers">3</button>
+          <button className="symbols">-</button>
+          <button className="numbers">.</button>
+          <button className="numbers">0</button>
+          <button className="symbols red">=</button>
+          <button className="symbols">+</button>
+        </section>
+      </div>
     </div>
   );
 }
