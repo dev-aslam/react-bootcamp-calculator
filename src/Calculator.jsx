1fc2e4ce3799566b146993d45f9fecd108142725
p-h-0-e-n-i-x <muhammedaslam204@gmail.com>
2022-11-04 23:57:58 +0530

    Added event listners for displaying i/p and o/p Created button for clearing i/o created button for Power on/off

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+
 function Calculator() {
 
+    const [input, setInput] = useState(0);
+    const [output, setOutput] = useState(0);
+    const [lastInput, setLastInput] = useState(false);
+    const [power, setPower] = useState(false);
+    let result=0;
     const inputs = [
         { value: 7, className: "numbers" },
         { value: 8, className: "numbers" },
@@ -15,23 +22,64 @@ function Calculator() {
         { value: "-", className: "symbols" },
         { value: ".", className: "numbers" },
         { value: 0, className: "numbers" },
-        { value: "=", className: "symbols green" },
+        { value: "=", className: "equals" },
         { value: "+", className: "symbols" },
     ]
+
+    function onInput(value, type) {
+        if (type === "equals") {
+            try {
+                result = eval(input);
+                setOutput(result);
+                return
+            } catch (error) {
+                alert("invalid input");
+                return
+            }
+        }
+        if (input === 0 && type === "numbers") {
+            setInput(value);
+            return
+        }
+        if (lastInput && type === "symbols") return;
+        setLastInput(type === "symbols");
+        setInput(input + "" + value);
+    }
+
+    function reset() {
+        setInput(0);
+        setOutput(0);
+        setLastInput(false);
+    }
+
+    function powerCheck() {
+        reset();
+        setPower(!power);
+    }
+
     return (
         < div className="container" >
             <section className="monitor">
-                <div className="input-data">
-                    25*25
-                </div>
-                <div className="result">
-                    625
-                </div>
+                {power &&
+                    <div className="input-data">
+                        {input}
+                    </div>}
+                {power &&
+                    <div className="result">
+                        {output}
+                    </div>}
             </section>
-            <section className="input-button">
+            <section className="controls">
+                <button className="control-btn" onClick={() => reset()}>CE</button>
+                <button className="control-btn" onClick={() => powerCheck()}>{power ? "OFF" : "ON"}</button>
+            </section>
+            <section className="input-buttons">
                 {inputs.map(
-                    ({ value, className }) => 
-                        <button className={className} key={value}>{value}</button>
+                    ({ value, className }) =>
+                        <button onClick={() => onInput(
+                            value,
+                            className
+                        )} className={className} key={value}>{value}</button>
                 )}
             </section>
         </div >
